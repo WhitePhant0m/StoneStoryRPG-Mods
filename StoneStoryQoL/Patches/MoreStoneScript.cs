6e3ac2d957cef40afa90853061298b69f99628c0
WhitePhant0m <kallunki.aleksi@gmail.com>
2026-03-02 09:35:00 +0200

    cleanup

--- StoneStoryQoL/Patches/MoreStoneScript.cs
+++ StoneStoryQoL/Patches/MoreStoneScript.cs
@@ -1,7 +1,7 @@
 ﻿using HarmonyLib;
 using Stonescript;
 using System.Collections.Generic;
-using System.Xml.Schema;
+using System.Linq;
 
 namespace StoneStoryQoL.Patches;
 
@@ -12,11 +12,11 @@ public static class MoreStoneScript {
         machine.RegisterFunction("loc.restart", RestartQuest);
 
         machine.RegisterVariable("foe.wakeupTics", delegate {
-            Enemy targetEnemy3 = GameStates.Singleton.hero.GetComponent<HeroAI>().targetEnemy;
+            var targetEnemy3 = GameStates.Singleton.hero.GetComponent<HeroAI>().targetEnemy;
             return (targetEnemy3 != null) ? new int?(targetEnemy3.wakeupTics) : null;
         });
         machine.RegisterVariable("foe.IsAwake", delegate {
-            Enemy targetEnemy4 = GameStates.Singleton.hero.GetComponent<HeroAI>().targetEnemy;
+            var targetEnemy4 = GameStates.Singleton.hero.GetComponent<HeroAI>().targetEnemy;
             return (targetEnemy4 != null) ? new bool?(targetEnemy4.IsAwake()) : null;
         });
         machine.RegisterVariable("player.level", () => XPController.singleton.currentLevel);
@@ -24,18 +24,15 @@ public static class MoreStoneScript {
         machine.RegisterFunction("treasure.openAll", TreasureOpen);
     }
 
-    public static object RestartQuest(List<object> parameters, InvocationContext ctx) {
+    private static object RestartQuest(List<object> parameters, InvocationContext ctx) {
         var gameState = GameStates.Singleton;
-        if (gameState.GetTotalTime() >= 120) {
-            Data.Quest quest = gameState.parentQuest ?? gameState.level.QuestData;
-            gameState.StartQuest(quest, true, true);
-            return true;
-        }
-
-        return false;
+        if (gameState.GetTotalTime() < 120) return false;
+        var quest = gameState.parentQuest ?? gameState.level.QuestData;
+        gameState.StartQuest(quest, true, true);
+        return true;
     }
 
-    public static object TreasureOpen(List<object> parameters, InvocationContext ctx) {
+    private static object TreasureOpen(List<object> parameters, InvocationContext ctx) {
         if (Inventory.Singleton.GetTreasures().Count == 0 ||
             GameStates.Singleton.CurrentState        == GameStates.State.PlayItemScreen) {
             return null;
@@ -43,20 +40,16 @@ public static class MoreStoneScript {
 
         GameStates.Singleton.SetState(GameStates.State.PlayItemScreen);
         var treasures = Inventory.Singleton.GetTreasures();
-        if (treasures.Count > 0) {
-            for (int i = 0; i < treasures.Count; i++) {
-                TreasureItem treasure = treasures[i];
-                if (treasure == null) continue;
-
-                GameStates.Singleton.itemScreen.SetState(ItemScreen.State.OpeningTreasure);
-                GameStates.Singleton.itemScreen.openTreasureDialog.Setup(treasure, true);
-                GameStates.Singleton.itemScreen.openTreasureDialog.autoSkip = true;
-                GameStates.Singleton.itemScreen.openTreasureDialog.Show();
-                AnalyticsMacros.OpenTreasure(treasure);
-            }
+        if (treasures.Count <= 0) return false;
+        foreach (var treasure in treasures.Where(treasure => treasure != null)) {
+            GameStates.Singleton.itemScreen.SetState(ItemScreen.State.OpeningTreasure);
+            GameStates.Singleton.itemScreen.openTreasureDialog.Setup(treasure, true);
+            GameStates.Singleton.itemScreen.openTreasureDialog.autoSkip = true;
+            GameStates.Singleton.itemScreen.openTreasureDialog.Show();
+            AnalyticsMacros.OpenTreasure(treasure);
         }
 
         GameStates.Singleton.SetState(GameStates.State.Playing);
-        return null;
+        return true;
     }
 }
